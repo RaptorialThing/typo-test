13a908a44f32e4a14d86d72289715432b071ae74
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2020-09-03 22:09:19 +0300

    delete old files

--- routesOldCustom.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "[a-z0-9_-]+" => "\Controller\PersonController::word",
-    "person\/insert\.*" => "\Controller\PersonController::createPerson",
-    "person\/update\/id\.*" => "\Controller\PersonController::updatePerson",
-];
